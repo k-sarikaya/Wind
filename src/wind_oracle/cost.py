ceef5b389c82f8d1c4362e1f28d5de9e06021eaa
Kadir SARIKAYA <kadir.sarikaya@gop.edu.tr>
2026-03-05 20:19:39 +0300

    Create cost.py

---
+++ src/wind_oracle/cost.py
@@ -0,0 +1,9 @@
+# Cost module for Wind Oracle
+
+class Cost:
+    def __init__(self, some_parameter):
+        self.parameter = some_parameter
+
+    def calculate_cost(self):
+        # Logic for calculating cost
+        pass
